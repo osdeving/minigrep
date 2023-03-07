caeadb6b03a92cbf7594ef5fe856c8f188db9658
Willams Sousa <osdeving@gmail.com>
2023-03-07 09:02:29 -0300

    refactor: :recycle: cria um módulo

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,9 @@
-use std::{env, fs, process};
+use std::{env, process};
 
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    // fn new(args: &[String]) -> Self {
-    //     if(args.len() < 3) {
-    //         panic!("no enough arguments");
-    //     }
-
-    //     let query = args[1].clone();
-    //     let file_path = args[2].clone();
-    
-    //     Self {query, file_path}
-    // }
-
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config {query, file_path})
-    }
-}
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
-    //let config = parse_config(&args);
 
     let config = Config::build(&args).unwrap_or_else(|err| {
         println!("problem parsing args: {}", err);
@@ -42,12 +13,8 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("in file {}", config.file_path);
 
-    let contents = fs::read_to_string(config.file_path).expect("msg");
-
-    println!("With text:\n{contents}");
+    if let Err(e) = minigrep::run(config) {
+        println!("application error: {}", e);
+        process::exit(1);
+    }
 }
-
-
-// fn parse_config(args: &[String]) -> Config {
-//     Config::build(args)
-// }
